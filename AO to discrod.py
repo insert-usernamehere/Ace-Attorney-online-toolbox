7fba3d032b3d38dc9c9341130d8f9d01688c01ef
insert-usernamehere <montyd18@gmail.com>
2020-10-17 10:14:37 -0400

    Update AO to discrod.py

--- AO to discrod.py
+++ AO to discrod.py
@@ -8,13 +8,22 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command()
 async def log(ctx):
-    with open('server.log') as f:
-        for line in f:
-            pass
-    last_line = line
-    sleep (2)
-    await ctx.send(last_line)
-    sleep (2)
+    while True:
+        with open('server.log') as f:
+            for line in f:
+                pass
+        last_line = line
+        m= open("lastline.txt","r")
+        oldline =m.read()
+        m.close() 
+        m= open("lastline.txt","w+")
+        m.write(last_line)
+        m.close()
+        if last_line == oldline:
+            pass 
+        else:
+            await ctx.send(last_line)
+            
         
     
-bot.run('bot-id')
+bot.run('NzY2NzUyNDY0ODM2NzU1NDY3.X4n74Q.bFMVRsAjiB4TofJ5z02fYQy5zpw')
