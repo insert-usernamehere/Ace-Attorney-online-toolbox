18ff693101f7f7f622c50b542fc481dd2310ec6a
insert-usernamehere <montyd18@gmail.com>
2020-10-18 15:40:54 -0400

    basically a rewrite

--- AO to discrod.py
+++ AO to discrod.py
@@ -1,33 +1,37 @@
 import discord
 from discord.ext import commands
 from time import sleep
+import os
 
-bot = commands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix='>')
 
 
-@bot.command()
-async def log(ctx):
-    while True:
-        with open('server.log') as f:
-            for line in f:
-                pass
-        last_line = line
-        m= open("lastline.txt","r")
-        oldline =m.read()
-        m.close() 
-        m= open("lastline.txt","w+")
-        m.write(last_line)
-        m.close()
-        if last_line == oldline:
-            pass
-        else:
-            await ctx.send(last_line)
+keepLooping = False
 
-@bot.command()
-async def stoplog(ctx):
-     await ctx.send("stoping logging and shutting down")
-     await ctx.bot.logout()
+@client.event
+async def on_message(message):
+    global keepLooping
+    if message.content.startswith("!log"):
+        keepLooping = True
+        while keepLooping:
+                    with open('server.log') as f:
+                            for line in f:
+                                pass
+                            last_line = line
+                            m= open("lastline.txt","r")
+                            oldline =m.read()
+                            m.close() 
+                            m= open("lastline.txt","w+")
+                            m.write(last_line)
+                            m.close()
+                            if last_line == oldline:
+                                pass
+                            elif last_line !=oldline:
+                                await message.channel.send(last_line)
+    elif message.content.startswith("!stoplog"):
+        keepLooping = False
+
     
 
     
-bot.run('botid')
+client.run('botid')
