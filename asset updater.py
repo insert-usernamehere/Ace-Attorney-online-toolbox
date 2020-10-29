041574e61d82f7b8be50545da15ac7db47960f1e
insert-usernamehere <montyd18@gmail.com>
2020-10-29 19:39:37 -0400

    Update asset updater.py

--- asset updater.py
+++ asset updater.py
@@ -2,11 +2,22 @@ import wget
 from zipfile import ZipFile
 import os
 
-url = 'https://dl-web.dropbox.com/zip_download_get/AlmbzsmQFJoXMDjatWC8GPEJF9_TlpMbJRxH7wuBHwYs-VU7VugCkBJz43JrtwzWbqMgITUDPgphN1_Ram7cGlw_jHL-GuLGZflGWsUu7FQppg?_download_id=9366712886235573678541395990133492636470298964969832627960112919&_notify_domain=www.dropbox.com'
-wget.download(url, 'AOmaster.zip')
+url ='https://insert-usernamehere.github.io/dblink.txt'
+wget.download(url, 'dblink.txt')
+
+f=open("dblink.txt", "r")
+db = f.read()
+
+
+wget.download(db, 'AOmaster.zip')
 zf = ZipFile('AOmaster.zip', 'r')
 zf.extractall('')
 zf.close()
+f.close
+if os.path.exists("dblink.txt"):
+  os.remove("dblink.txt")
+else:
+  pass
 if os.path.exists("AOmaster.zip"):
   os.remove("AOmaster.zip")
 else:
