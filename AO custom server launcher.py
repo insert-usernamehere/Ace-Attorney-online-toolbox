7bcdb96f6ef50657d9089b99cc812cd5949d67de
insert-usernamehere <montyd18@gmail.com>
2020-10-29 17:04:47 -0400

    Update AO custom server launcher.py

--- AO custom server launcher.py
+++ AO custom server launcher.py
@@ -6,6 +6,6 @@ if os.path.exists("base/serverlist.txt"):
   os.remove("base/serverlist.txt")
 else:
   pass
-url = 'https://insert-usernamehere.github.io/severlist.txt'
+url = 'https://insert-usernamehere.github.io/serverlist.txt'
 wget.download(url, 'base/serverlist.txt')
 os.startfile("Attorney_Online.exe")
