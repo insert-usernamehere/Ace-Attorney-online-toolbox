67b775f2e3dc14775c211abcd158ac77f5245c40
insert-usernamehere <montyd18@gmail.com>
2020-10-30 09:18:34 -0400

    Update asset updater.py

--- asset updater.py
+++ asset updater.py
@@ -7,13 +7,12 @@ wget.download(url, 'dblink.txt')
 
 f=open("dblink.txt", "r")
 db = f.read()
-
+f.close()
 
 wget.download(db, 'AOmaster.zip')
 zf = ZipFile('AOmaster.zip', 'r')
 zf.extractall('')
 zf.close()
-f.close
 if os.path.exists("dblink.txt"):
   os.remove("dblink.txt")
 else:
