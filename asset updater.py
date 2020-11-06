573275fd0b5778b354d24d519b6b92a3133806dd
insert-usernamehere <montyd18@gmail.com>
2020-11-05 20:29:21 -0500

    Update asset updater.py

--- asset updater.py
+++ asset updater.py
@@ -2,22 +2,41 @@ import wget
 from zipfile import ZipFile
 import os
 
-url ='https://insert-usernamehere.github.io/dblink.txt'
-wget.download(url, 'dblink.txt')
+url ='https://insert-usernamehere.github.io/soudblink.txt'
+wget.download(url, 'soudblink.txt')
 
-f=open("dblink.txt", "r")
+f=open("soudblink.txt", "r")
 db = f.read()
 f.close()
 
-wget.download(db, 'AOmaster.zip')
-zf = ZipFile('AOmaster.zip', 'r')
-zf.extractall('')
+wget.download(db, 'sounds.zip')
+zf = ZipFile('sounds.zip', 'r')
+zf.extractall('base')
 zf.close()
-if os.path.exists("dblink.txt"):
-  os.remove("dblink.txt")
+if os.path.exists("soudblink.txt"):
+  os.remove("soudblink.txt")
 else:
   pass
-if os.path.exists("AOmaster.zip"):
-  os.remove("AOmaster.zip")
+if os.path.exists("sounds.zip"):
+  os.remove("sounds.zip")
+else:
+  pass
+url ='https://insert-usernamehere.github.io/chrdblink.txt'
+wget.download(url, 'chrdblink.txt')
+
+f=open("chrdblink.txt", "r")
+db = f.read()
+f.close()
+
+wget.download(db, 'characters.zip')
+zf = ZipFile('characters.zip', 'r')
+zf.extractall('base')
+zf.close()
+if os.path.exists("chrdblink.txt"):
+  os.remove("chrdblink.txt")
+else:
+  pass
+if os.path.exists("characters.zip"):
+  os.remove("characters.zip")
 else:
   pass
