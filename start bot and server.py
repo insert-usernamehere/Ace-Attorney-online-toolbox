7792eeaf5bf93dc1a70e0e10e66a528c9c91f854
insert-usernamehere <montyd18@gmail.com>
2020-12-20 13:51:50 -0500

    Update start bot and server.py

--- start bot and server.py
+++ start bot and server.py
@@ -1,6 +1,10 @@
 import os
 import time
+import sys
 
 os.startfile('AO to discord.py')
 time.sleep(0.7)
 os.startfile('start_server.py')
+print("both programs have started successfully this window will now close")
+time.sleep(0.5)
+sys.exit()
