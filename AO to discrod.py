b168b3a30db7bc6b955f1dcc2e31c479b6017bbf
insert-usernamehere <montyd18@gmail.com>
2020-12-19 19:15:47 -0500

    added automated file clearing

--- AO to discrod.py
+++ AO to discrod.py
@@ -2,6 +2,14 @@ import discord
 from discord.ext import commands
 from time import sleep
 import os
+import datetime
+
+
+if os.path.exists("logs/server.log"):
+    Current_Date = datetime.datetime.today().strftime(" %d %m %Y %H %M")
+    os.rename('logs/server.log','logs/old server log' + str(Current_Date) + '.log')
+else:
+    pass
 
 client = commands.Bot(command_prefix='!')
 
@@ -14,14 +22,14 @@ async def on_message(message):
     if message.content.startswith("!log"):
         keepLooping = True
         while keepLooping:
-                    with open('server.log') as f:
+                    with open('logs/server.log') as f:
                             for line in f:
                                 pass
                             last_line = line
-                            m= open("lastline.txt","r")
+                            m= open("logs/lastline.txt","r")
                             oldline =m.read()
                             m.close() 
-                            m= open("lastline.txt","w+")
+                            m= open("logs/lastline.txt","w+")
                             m.write(last_line)
                             m.close()
                             if last_line == oldline:
