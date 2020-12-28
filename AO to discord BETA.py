f7d73669d7032aa090643e51ae5aac81fe41baa2
insert-usernamehere <montyd18@gmail.com>
2020-12-28 12:51:39 -0500

    Update AO to discord BETA.py

--- AO to discord BETA.py
+++ AO to discord BETA.py
@@ -29,18 +29,18 @@ async def on_message(message):
                         f.seek(-2, os.SEEK_END)
                         while f.read(1) != b'\n':
                             f.seek(-2, os.SEEK_CUR)
-                        lastline = f.readline().decode()
-                             m= open("logs/lastline.txt","r")
-                             oldline =m.read()
-                              m.close() 
-                              m= open("logs/lastline.txt","w+")
-                              m.write(last_line)
-                              m.close()
-                              if last_line == oldline:
-                                 pass
-                               elif last_line !=oldline:
-                                    await message.channel.send(last_line)
-     elif message.content.startswith("!stoplog"):
+                          lastline = f.readline().decode()
+                        m= open("logs/lastline.txt","r")
+                        oldline =m.read()
+                        m.close() 
+                        m= open("logs/lastline.txt","w+")
+                        m.write(last_line)
+                        m.close()
+                          if last_line == oldline:
+                                pass
+                        elif last_line !=oldline:
+                          await message.channel.send(last_line)
+    elif message.content.startswith("!stoplog"):
             keepLooping = False
 
     
