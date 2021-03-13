8f9cd79d7edfc45d534a3589cf9dfb4fd5bfaefe
insert-usernamehere <montyd18@gmail.com>
2021-03-13 11:23:46 -0500

    Update AO launcher and asset updater.py

--- AO launcher and asset updater.py
+++ AO launcher and asset updater.py
@@ -38,6 +38,35 @@ def DLCHR():
   except Exception:
         T.delete(0, tkinter.END)
         T.insert(0, "oh no! an error occoured this might be due to the server being down please wait 5 minutes and try again. DEBUG INFO: exception on process DLCHR")
+def DLCORT():
+  T.delete(0, tkinter.END)
+  T.insert(0, "now downloading courtrooms please wait")
+  try:
+      if os.path.exists("base"):
+        pass
+      else:
+        os.mkdir("base")
+      if os.path.exists("base/background"):
+        pass
+      else:
+        os.mkdir("base/background")
+      wget.download('http://fierce-push.auto.playit.gg:53368/courtrooms.zip', 'courtrooms.zip')
+      zf = ZipFile('courtrooms.zip', 'r')
+      zf.extractall('base/background')
+      zf.close()
+      if os.path.exists("cortdblink.txt"):
+        os.remove("cortdblink.txt")
+      else:
+        pass
+      if os.path.exists("courtrooms.zip"):
+        os.remove("courtrooms.zip")
+      else:
+        pass
+      T.delete(0, tkinter.END)
+      T.insert(0, "Courtroom downloading finished")
+  except Exception:
+        T.delete(0, tkinter.END)
+        T.insert(0, "oh no! an error occoured this might be due to the server being down please wait 5 minutes and try again. DEBUG INFO: exception on process DLCORT")
 def DLSOU():
   if os.path.exists("base"):
     pass
@@ -95,7 +124,7 @@ def startAO():
 def startAOA():
   T.delete(0, tkinter.END)
   T.insert(0, "Doing everything please wait")
-  time.sleep(89)
+  time.sleep(97)
   try:
     os.startfile("Attorney_Online.exe")
     T.delete(0, tkinter.END)
@@ -108,6 +137,11 @@ def DLCHRB():
   x = threading.Thread(target=DLCHR)
   x.daemon = True
   x.start()
+def DLCORTB():
+  global z
+  z = threading.Thread(target=DLCORT)
+  z.daemon = True
+  z.start()
 def DLSOUB():
   global j
   j = threading.Thread(target=DLSOU)
@@ -128,6 +162,7 @@ def ALL():
   global a2
   global a3
   global a4
+  global a5
   a1 = threading.Thread(target=DLCHR)
   a1.daemon = True
   a1.start()
@@ -137,6 +172,9 @@ def ALL():
   a3 = threading.Thread(target=serveradd)
   a3.daemon = True
   a3.start()
+  a5 = threading.Thread(target=DLCORT)
+  a5.daemon = True
+  a5.start()
   a4 = threading.Thread(target=startAOA)
   a4.daemon = True
   a4.start()
@@ -151,12 +189,14 @@ T.delete(0, tkinter.END)
 T.insert(0, "Welcome to the AO launcher")
   
 A = tkinter.Button(top, text ="download characters", command = DLCHRB)
+L = tkinter.Button(top, text ="download courtrooms", command = DLCORTB)
 B = tkinter.Button(top, text ="download sounds", command = DLSOUB)
 D = tkinter.Button(top, text ="start AO", command = startAOB)
 C = tkinter.Button(top, text ="add best bois courthouse server", command = serveraddB)
 E = tkinter.Button(top, text ="Do it all", command = ALL)
 F = tkinter.Button(top, text ="kill thread (debug)", command = on_closing)
 A.pack()
+L.pack()
 B.pack()
 C.pack()
 D.pack()
